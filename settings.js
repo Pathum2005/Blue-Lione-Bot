420f27abd40eba0a4b88b5f25ad009a0990963a7
Pathum2005 <114092571+Pathum2005@users.noreply.github.com>
2022-09-21 08:52:03 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 
 
 global.OWNER_REACT = '🧑🏻‍💻'
-global.REACT_NUMBER = '94714188730'
+global.REACT_NUMBER = '+94787815824'
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -34,32 +34,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "NIPUNA " //ur owner name
-global.ytname = "YT: Nipuna-apps" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['+94787815824'] //ur owner number
+global.ownername = "Pathum " //ur owner name
+
 
 //bot bomdy 
-global.owner = ['714188730']
-global.ownertag = '714188730' //ur tag number
-global.botname = '⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜' //ur bot name
-global.linkz = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //your theme url which will be displayed on whatsapp
-global.websitex = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //ur website to be displayed
-global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot.git' //script link
+global.owner = ['+94787815824']
+global.ownertag = '+94787815824' //ur tag number
+global.botname = '⚜Pathum BOT⚜' //ur bot name
+
 global.reactmoji = "❤" //ur menu react emoji
 global.themeemoji = "🎸" //ur theme emoji
-global.packname = "⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "NIPUNA" //ur sticker watermark author
+global.packname = "⚜Pathum BOT⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "Pathum BOT" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("https://imgs.search.brave.com/z2y2qIVJgdO5BoZl5T3bTUqNBJNVkP1RTN5BMTZymY0/rs:fit:1200:811:1/g:ce/aHR0cHM6Ly93d3cu/Y29yZW1hZmlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy93aGF0c2Fw/cC1ib3QuanBn") //ur thumb pic
+global.log0 = fs.readFileSync("https://imgs.search.brave.com/z2y2qIVJgdO5BoZl5T3bTUqNBJNVkP1RTN5BMTZymY0/rs:fit:1200:811:1/g:ce/aHR0cHM6Ly93d3cu/Y29yZW1hZmlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy93aGF0c2Fw/cC1ib3QuanBn") //ur logo pic
+global.err4r = fs.readFileSync("https://imgs.search.brave.com/z2y2qIVJgdO5BoZl5T3bTUqNBJNVkP1RTN5BMTZymY0/rs:fit:1200:811:1/g:ce/aHR0cHM6Ly93d3cu/Y29yZW1hZmlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy93aGF0c2Fw/cC1ib3QuanBn") //ur error pic
+global.thumb = fs.readFileSync("https://imgs.search.brave.com/z2y2qIVJgdO5BoZl5T3bTUqNBJNVkP1RTN5BMTZymY0/rs:fit:1200:811:1/g:ce/aHR0cHM6Ly93d3cu/Y29yZW1hZmlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy93aGF0c2Fw/cC1ib3QuanBn") //ur thumb pic
 
 //damtabase
-global.premium = ['714188730'] //ur premium numbers
+global.premium = ['+94787815824'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -113,7 +109,7 @@ global.mess = {
    umpanawal: 5,
    potionawal: 1
 }
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.thumb = fs.readFileSync('https://imgs.search.brave.com/z2y2qIVJgdO5BoZl5T3bTUqNBJNVkP1RTN5BMTZymY0/rs:fit:1200:811:1/g:ce/aHR0cHM6Ly93d3cu/Y29yZW1hZmlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy93aGF0c2Fw/cC1ib3QuanBn')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
